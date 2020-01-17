d4c54ba0d85f9531d88b735356d64fda36400a89
Nikola <nidza.bg@gmail.com>
2020-01-17 21:34:46 +0100

    Version 0.01

---
+++ src/Bladecomponents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace dzonibg\bladecomponents;
+
+class Bladecomponents {
+    public function confirm() {
+        return "The blade components package is installed.";
+        }
+}
+
+
